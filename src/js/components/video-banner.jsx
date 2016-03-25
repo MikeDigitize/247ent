4a69adcd4fb96a2bdf18a9823dab42cf0f3a0cdb
Mike Chadwick <chad_01@hotmail.co.uk>
2016-03-25 21:58:57 +0000

    Video banner ok sorry ie10

--- src/js/components/video-banner.jsx
+++ src/js/components/video-banner.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import "video.js";
+import { debounce } from "../utils/dom-utils";
 
 export default class VideoBanner extends Component {
     constructor() {
         super();
+        this.state = {
+            wrapperPadding : 0,
+            display : "none"
+        };
+    }
+    componentDidMount() {
+        this.refs.videoBanner.addEventListener("loadedmetadata", () => {
+            this.setWrapperPadding(this.refs.videoBanner.videoHeight / this.refs.videoBanner.videoWidth * 100);
+        });
+    }
+    setWrapperPadding(pb) {
+        this.setState({
+            wrapperPadding : pb,
+            display : "block"
+        });
     }
     render() {
         return (
-            <div className="banner-wrapper">
-                <video id="video-banner" className="video-js ambient" loop autoPlay>
+            <div className="banner-wrapper" style={{ paddingBottom : this.state.wrapperPadding + "%", display: this.state.display }}>
+                <video id="video-banner" loop autoPlay ref="videoBanner">
                     <source src="../media/video/video-banner.mp4" type="video/mp4" />
                     <source src="../media/video/video-banner.webm" type="video/webm" />
                     <source src="../media/video/video-banner.ogv" type="video/ogg" />
